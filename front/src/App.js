30c310a9c1244e73a9c4f7bb3f076a8cb562e5c8
Kwaku Antwi Addo <kaddo@whispeak.io>
2021-11-30 13:55:31 +0100

    [FEAT] Reat routers, initial pages included

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
+import { React, useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import axios from 'axios'
+import Navbar from './navbar';
+import Home from './home'
+import LoginUsers from './components/login/login-users';
+import LoginProfessionals from './components/login/login-pros';
 
 function App() {
+  const [data, setData] = useState(null);
 
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+  useEffect(() => {
+    axios.get("/api")
+      .then((res) => {
+        setData(res.data.message)
+      })
+      .catch(err => console.log(err))
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <Navbar></Navbar>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route path="/login" element={<LoginUsers />}/>
+            <Route path="/login-pro" element={<LoginProfessionals />}/>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
