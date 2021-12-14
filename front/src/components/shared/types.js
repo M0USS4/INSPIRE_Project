bdb4c3f3d993d03d42c669a187f7ec867bde91d2
Kwaku Antwi Addo <kaddo@whispeak.io>
2021-12-14 21:45:17 +0100

    [META] helper files update

---
+++ front/src/components/shared/types.js
@@ -0,0 +1,17 @@
+export const ClickType = {
+  block: {
+    type: 'block',
+    title: 'Are you sure you want to block this time?',
+    subtitle: 'You can unblock anytime'
+  },
+  unblock: {
+    type: 'unblock',
+    title: 'Are you sure you want to unblock this time?',
+    subtitle: 'You can block anytime'
+  },
+  cancel: {
+    type: 'cancel',
+    title: 'Are you sure you want to cancel this time?',
+    subtitle: 'Please note this cannot be reversed'
+  }
+};
