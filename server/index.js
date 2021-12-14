71ef51e2293f7c480a0514d99323c4ba04cb340b
Kwaku Antwi Addo <kaddo@whispeak.io>
2021-12-14 21:45:53 +0100

    [FEAT] cors library added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 const mysql = require("mysql")
 
