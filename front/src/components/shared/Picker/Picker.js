2ee798c30527ea37d2a832cf887e8f177e58a7d7
Kwaku Antwi Addo <kaddo@whispeak.io>
2022-01-12 20:07:33 +0100

    [FEAT] picker update and rename

--- front/src/components/shared/Picker/Picker.js
+++ front/src/components/shared/Picker/Picker.js
@@ -1,49 +1,40 @@
 import {React, useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-
-import data from '../../data/data';
 import './Picker.css';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { IconButton, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, IconButton, Typography } from '@mui/material';
 
-const Picker = ({ practicianData, columns, handleRdv}) => {
+const Picker = ({ practicianData, columns, handleRdv, selectedType, eventList}) => {
   const [dates, setdates] = useState([]);
-  const [displayNumber, setdisplayNumber] = useState(0);
-  const [responsiveNumber, setresponsiveNumber] = useState(7);
+  const [page, setpage] = useState(0);
+  const numberOfColumns = columns? columns : 7;
   const [filter, setfilter] = useState(4);
 
   useEffect(() => {
-    getDates(7);
-    const number = columns? columns : 7;
-    setresponsiveNumber(number);
-  }, [displayNumber, responsiveNumber]);
+    getDates();
+  }, [page, numberOfColumns, eventList]);
 
   const checkReserved = (time) => {
-
-    const find = data.some(event => {
+    return eventList.some(event => {
       return +event.start === +time.date;
     });
-    return find;
+
   };
 
   const nextDate = () => {
-    setdisplayNumber(displayNumber + responsiveNumber);
-    // getDates();
+    setpage(page + numberOfColumns);
   };
 
   const prevDate = () => {
-    if(displayNumber > 0){
-      setdisplayNumber(displayNumber - responsiveNumber);
+    if(page > 0){
+      setpage(page - numberOfColumns);
     }
-    // getDates();
   };
 
   const handleMoreTimes = () => {
-    console.log(dates);
     const maxTimeSlot = Math.max(...dates.map(date => date.timeslots.length));
-    console.log(maxTimeSlot);
     setfilter(maxTimeSlot);
   };
 
@@ -56,10 +47,12 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
     const dayOfweek = current.format('dddd').toLowerCase();
     const year  = current.format('YYYY');
     console.log(dayOfweek);
-    practicianData.availability.find(available => available.id === dayOfweek).availability.map(available => {
+    const practicianAvailability = JSON.parse(practicianData.availability);
+    practicianAvailability.find(available => available.id === dayOfweek).availability.map(available => {
       for(let k = available.start; k <= available.end; k++){
         times.push({
-          raw: k+':00',
+          raw: k,
+          formatted: k+':00',
           date: new Date(year, month - 1  , day, k, 0, 0),
           disabled: checkReserved({date: new Date(year, month - 1  , day  , k, 0, 0)})
         } );
@@ -69,7 +62,7 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
     date ={
       format: current.format('DD MMM'),
       day: current.format('dddd'),
-      date: current.format('dddd'),
+      full: current.format('YYYY-MM-DD'),
       timeslots: times.filter(time => time.disabled === false)
     };
     return date;
@@ -77,11 +70,11 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
   const getDates = () => {
     const currentDate = moment();
     const today = moment();
-    const weekStart = currentDate.clone().weekday(displayNumber);
+    const weekStart = currentDate.clone().weekday(page);
     let days = [];
     let current;
 
-    switch(responsiveNumber){
+    switch(numberOfColumns){
     case 7:
       for (let i = 0; i <= 6; i++) {
         current = moment(weekStart).add(i, 'days');
@@ -91,14 +84,14 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
       break;
     case 4:
       for (let i = 0; i <= 3; i++) {
-        current = moment(today).add((displayNumber + i), 'days');
+        current = moment(today).add((page + i), 'days');
         const date = handleAvailability(current);
         days.push(date);
       }
       break;
     case 3:
-      for (let i = -1; i <= 1; i++) {
-        current = moment(today).add((displayNumber + i), 'days');
+      for (let i = 0; i <= 2; i++) {
+        current = moment(today).add((page + i), 'days');
         const date = handleAvailability(current);
         days.push(date);
       }
@@ -116,23 +109,12 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
 
   return (
     <div className="k-timer-container">
-      {/* <div className="k-picker-button">
-        <IconButton onClick={prevDate} size="large" className="picker-button">
-          <ArrowBackIosIcon fontSize="inherit"/>
-        </IconButton>
-        <IconButton onClick={nextDate} size="large">
-          <ArrowForwardIosIcon fontSize="inherit"/>
-        </IconButton>
-      </div> */}
       <div className="k-timer">
         <IconButton onClick={prevDate} size="large" className="k-picker-button">
           <ArrowBackIosIcon fontSize="inherit"/>
         </IconButton>
-        {dates.map(day => (
+        {(selectedType) && dates.map(day => (
           <div key={day.format} className="k-picker-content">
-            {/* <h3 >{day.day.slice(0, 3)}</h3>
-            <span >{day.format}</span> */}
-
             <Typography variant="subtitle1" >
               {day.day.slice(0, 3)}
             </Typography>
@@ -143,9 +125,11 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
               {
                 day.timeslots.slice(0,filter).map(time => (
                   !time.disabled &&
-                      <button key={time.raw}
-                        disabled={time.disabled}
-                        onClick={() => handleRdv(practicianData, day.day, day.format, time.raw)}>{time.raw}</button>
+                  <button key={time.raw}
+                    disabled={time.disabled}
+                    onClick={() => handleRdv(practicianData, day.day, day.format, time.raw, time.formatted, day.full)}>
+                    {time.formatted}
+                  </button>
                 ))
               }
               {
@@ -156,13 +140,36 @@ const Picker = ({ practicianData, columns, handleRdv}) => {
             </div>
           </div>
         ))}
+        {!selectedType && dates.map(day => (
+          <div key={day.format} className="k-picker-content">
+            <Typography variant="subtitle1" >
+              {day.day.slice(0, 3)}
+            </Typography>
+            <Typography variant="subtitle2" >
+              {day.format}
+            </Typography>
+          </div>
+        ))}
         <IconButton onClick={nextDate} size="large" className="k-picker-button">
           <ArrowForwardIosIcon fontSize="inherit"/>
         </IconButton>
       </div>
-      <div className="picker-more">
-        {filter <= 4 && <button className="picker-more-btn" onClick={handleMoreTimes}>See More Times</button>}
+      <Box sx={{ display: selectedType? 'none' : 'flex', justifyContent: 'center' }}>
+        <CircularProgress />
+      </Box>
+      <div  style={{ display: selectedType? 'block' : 'none' }} className="picker-more">
+        {filter <= 4 &&
+        <Button
+          className="picker-more-btn"
+          onClick={handleMoreTimes}
+          sx={{
+            color: 'text.primary',
+            borderRadius: '0px'
+
+          }}
+        >See More Times</Button>}
       </div>
+
     </div>
   );
 };
@@ -171,7 +178,9 @@ Picker.propTypes = {
   filter: PropTypes.number,
   handleRdv: PropTypes.func,
   practicianData: PropTypes.object,
-  columns: PropTypes.number
+  columns: PropTypes.number.isRequired,
+  selectedType: PropTypes.string.isRequired,
+  eventList: PropTypes.array
 };
 
 export default Picker;
