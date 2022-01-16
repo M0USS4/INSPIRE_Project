87302fec65617021eeb224bd9c397daaa9232548
Kwaku Antwi Addo <kaddo@whispeak.io>
2022-01-16 04:32:01 +0100

    [FEAT] login update

--- front/src/components/login/login-users.js
+++ front/src/components/login/login-users.js
@@ -2,43 +2,8 @@
 import {React} from 'react';
 import Login from './login';
 import './login.css';
-// import axios from 'axios';
-// import { useForm } from 'react-hook-form';
-// import { useNavigate } from 'react-router';
-// import CollapsableAlert from '../shared/CollapsableAlert';
 
 const LoginUsers = () => {
-  // let navigate = useNavigate();
-  // const [success, setsuccess] = useState(null);
-  // const [message, setmessage] = useState('');
-  // const [open, setopen] = useState(false);
-
-  // const { register, handleSubmit, formState: { errors } } = useForm();
-  // const onSubmit = (data) =>{
-  //   console.log(data);
-  //   const loginData = {
-  //     login: {
-  //       mail: data.email,
-  //       password: data.password,
-  //     }
-  //   };
-  //   axios.post('http://localhost:2021/login/post', loginData)
-  //     .then(response => {
-  //       console.log(response);
-  //       console.log(success);
-  //       setsuccess(true);
-  //       navigate('/pro-profile',      { state: {
-  //         success: success,
-  //       }});
-
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //       setopen(true);
-  //       setmessage(err.data);
-  //       setsuccess(false);
-  //     });
-  // };
   return (
     <section className="user-login">
       <div className="login">
@@ -48,18 +13,12 @@ const LoginUsers = () => {
                         Welcome
             </h1>
           </div>
-          {/* <CollapsableAlert
-            open={open}
-            setopen={setopen}
-            message={message}
-            severity={'success'}
-          /> */}
           <div className="login-fields">
             <Login/>
             <div className="buttons">
               <div className="new-user">
                 <p>New to Inspire?</p>
-                <a href="" className="button2"> Creer un Compte</a>
+                <a href="/register" className="button2"> Creer un Compte</a>
               </div>
             </div>
           </div>
