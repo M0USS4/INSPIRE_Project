39d0e80f857810e9141ccfa352d96c8d8de76806
Kwaku Antwi Addo <kaddo@whispeak.io>
2022-01-16 04:45:34 +0100

    [FEAT] 404 positioning

--- front/src/components/shared/404.js
+++ front/src/components/shared/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 import imgNotFound from '../../images/404-error-animate.svg';
 const NotFound = () => {
   return (
-    <div style={{display: 'flex'}}>
-      <img src={imgNotFound} alt="" />
+    <div style={{display: 'flex', justifyContent:'center', alignContent: 'center', height:'100%'}}>
+      <img src={imgNotFound} alt="" style={{maxHeight: '600px'}}/>
     </div>
   );
 };
